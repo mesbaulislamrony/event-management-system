7f0f0a02ac43c9a725fda897572ef555442c4123
mesbaulislamrony <mesbaul.cse26@gmail.com>
2025-01-22 19:05:45 +0600

    move to root dir

--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('layouts/header.php');
-?>
-<h1>Home Page</h1>
-<?php
-include('layouts/footer.php');
-?>
--- public/layouts/header.php
+++
@@ -1,4 +0,0 @@
-<a href="index.php">Home</a>
-<a href="/auth/login.php">Login</a>
-<a href="/events/index.php">Events</a>
-<a href="/auth/register.php">Register</a>
